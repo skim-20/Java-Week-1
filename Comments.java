2ef24254f7c075b772c176d19b21d7fb418d1ce5
skim-20 <ssk456999@gmail.com>
2025-02-11 10:08:31 -0500

    Create Comments.java

---
+++ Comments.java
@@ -0,0 +1,12 @@
+/* Samantha 2/11/25 */
+
+
+
+public class Comments
+{
+public static void main (String[]args)
+{
+System.out.println("Hi"); //message to user
+System.out.println("LabOneA");//assignment
+}
+}
